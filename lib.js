a6984c529906d89ca5297bb36a5d64f767e783df
yayaluoya <yayaluoya@sina.com>
2024-09-15 21:50:01 +0800

    配置打包lib

---
+++ lib.js
@@ -0,0 +1,6 @@
+export * from "./src/components/dynamic-form/ConT";
+export { default as vue3EditableForm } from "./src/components/dynamic-form/index.vue";
+export { default as vue3EditableFormRender } from "./src/components/dynamic-form/render.vue";
+export * from "./src/components/dynamic-form/controls/index";
+export * from "./src/components/dynamic-form/com/FontStyleCon";
+export * from "./src/components/dynamic-form/com/FormItemCon";
