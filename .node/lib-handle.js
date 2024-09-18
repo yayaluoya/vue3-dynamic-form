a82d1bfbdc2618556637e06dcf3ee3d7e07aeae5
yayaluoya <yayaluoya@sina.com>
2024-09-18 16:07:43 +0800

    -

--- .node/lib-handle.js
+++ .node/lib-handle.js
@@ -4,6 +4,27 @@ import { fileURLToPath } from "node:url";
 
 let dirname = path.dirname(fileURLToPath(import.meta.url));
 
-fs.createReadStream(path.join(dirname, "../lib-package.json")).pipe(
-  fs.createWriteStream(path.join(dirname, "../dist-lib/package.json"))
-);
+[
+  {
+    from: path.join(dirname, "../lib-package.json"),
+    to: path.join(dirname, "../dist-lib/package.json"),
+  },
+].forEach((_) => {
+  copyFile(_.from, _.to);
+});
+
+function copyFile(from, to) {
+  let state = fs.statSync(from, {
+    throwIfNoEntry: false,
+  });
+  if (state?.isFile()) {
+    fs.createReadStream(from).pipe(fs.createWriteStream(to));
+  } else if (state?.isDirectory()) {
+    try {
+      fs.mkdirSync(to, { recursive: true });
+    } catch {}
+    fs.readdirSync(from).forEach((_) => {
+      copyFile(path.join(from, _), path.join(to, _));
+    });
+  }
+}
